86a6f988dec1c23b3d1b036b9c8ead6256935657
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-11 18:55:25 -0400

    Implemented war-ordering (Ace-high) in a Comparator.

--- src/main/java/edu/cnm/deepdive/Test.java
+++ src/main/java/edu/cnm/deepdive/Test.java
@@ -6,6 +6,7 @@ import edu.cnm.deepdive.model.Suit;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Test {
@@ -24,10 +25,22 @@ public class Test {
     Collections.shuffle(deck, new SecureRandom());
     System.out.println(deck);
     //sort the deck
-    Collections.sort(deck); // or List.sort(deck, null)
+    Collections.sort(deck); // or deck.sort(deck, null)
+    System.out.println(deck);
+    //sort in war order, use WarComparator as referee
+    deck.sort(new WarComparator());
     System.out.println(deck);
   }
 
+  private static class WarComparator implements Comparator<Card> {
 
+    private final int numRanks = Rank.values().length;
+
+    //could do some math: add 12 to ordinal enum, then mod 13. always sort smaller member before larger one
+    @Override
+    public int compare(Card card1, Card card2) {
+      return (card1.getRank().ordinal() + numRanks-1) % numRanks - (card2.getRank().ordinal() + numRanks-1) % numRanks;
+    }
+  }
 
 }
