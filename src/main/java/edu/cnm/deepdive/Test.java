fe3a3fcbf00a79be50068056e4f940ae9592fdf3
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-11 17:05:48 -0400

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/Test.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive;
+
+import edu.cnm.deepdive.model.Card;
+import edu.cnm.deepdive.model.Rank;
+import edu.cnm.deepdive.model.Suit;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+  public static void main(String[] args) {
+    List<Card> deck = new ArrayList<>();
+    for (Suit suit: Suit.values()) {
+      for (Rank rank: Rank.values()) {
+        Card card = new Card(rank, suit);
+        deck.add(card);
+      }
+    }
+    System.out.println(deck);
+  }
+
+}
