860be43017b1222d03028e551137441dd302262e
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-22 12:07:11 -0500

    Trick implementation (in progress).

---
+++ src/main/java/edu/cnm/deepdive/Trick.java
@@ -0,0 +1,64 @@
+package edu.cnm.deepdive;
+
+import edu.cnm.deepdive.model.Card;
+import edu.cnm.deepdive.model.Rank;
+import edu.cnm.deepdive.model.Suit;
+import edu.cnm.deepdive.model.Suit.Color;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class Trick {
+
+  public static void main(String[] args) {
+    Random rng = new SecureRandom(); //good for gaming.
+    // Create a shuffled deck.
+    List<Card> deck = getDeck(rng);
+    List<Card> redPile = new ArrayList<>();
+    List<Card> blackPile = new ArrayList<>();
+    // Sort the deck into 2 piles, according to the rules of the trick.
+    sortCards(deck, redPile, blackPile);
+    // Swap a random number of cards between the 2 piles, take from the smaller of the two piles.
+    swapCards(redPile,blackPile,rng);
+    // TODO: Sort the piles.
+
+    // TODO: Count the number of pile contents.
+
+  }
+
+  private static List<Card> getDeck(Random rng) {
+    List<Card> deck = new ArrayList<>();//want to be able to add at index poins very easily
+    for (Suit suit : Suit.values()) {
+      for (Rank rank: Rank.values()) {
+        deck.add(new Card(rank, suit));
+      }
+    }
+    //every rank is in a suit, but not vice versa.
+    Collections.shuffle(deck, rng);//using secure random
+    return deck;
+  }
+
+  private static void sortCards(List<Card> deck, List<Card> redPile, List<Card> blackPile) {
+    for (Iterator<Card> iter = deck.iterator(); deck.iterator().hasNext();) {
+      Card selector = iter.next();
+      Card card = iter.next();
+      if (selector.getSuit().getColor() == Color.RED) {
+        redPile.add(card);
+      } else { // else the color is BLACK
+        blackPile.add(card);
+      }
+    }
+  }
+
+  private static void swapCards(List<Card> redPile, List<Card> blackPile, Random rng) {
+    int numToSwap = rng.nextInt(Math.min(redPile.size(), blackPile.size()) + 1);
+    for (int i = 0; i < numToSwap; i++) {
+      blackPile.add(redPile.remove(0)); //adds to end/bottom of the pile
+      redPile.add(blackPile.remove(0));
+    }
+  }
+
+}
